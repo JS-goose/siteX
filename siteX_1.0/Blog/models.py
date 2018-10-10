004cd468bdda068187a1893a14c40e7a60e7a82a
spil3141 <johnj2015.jj22@gmail.com>
2018-10-11 00:24:33 +0900

    Update models.py

--- siteX_1.0/Blog/models.py
+++ siteX_1.0/Blog/models.py
@@ -1,20 +1,21 @@
 from django.db import models
 
 # Create your models here.
-
-class blog_content(models.Model):
-    blog_id = models.IntegerField(primary_key = True)
-    blog_text = models.TextField()
-
-class favorite_information(models.Model):
-    favorite_id= models.IntegerField(primary_key = True)
-    favorite_blog_id = models.ForeignKey(blog_content, on_delete = models.CASCADE)
-    
+   
 class account_information(models.Model):
     user_id = models.CharField(max_length=20, primary_key = True)
     user_password = models.CharField(max_length=30)
-    favorites = models.ForeignKey(favorite_information,on_delete = models.CASCADE)
-    user_blog_id = models.ForeignKey(blog_content, on_delete = models.CASCADE)
+    
+class blog_content(models.Model):
+    blog_id = models.IntegerField(primary_key = True)
+    blog_text = models.TextField()
+    blog_user_id = models.ForeignKey(account_information,on_delete = models.CASCADE)
+    
+
+class favorite_information(models.Model):
+    user_favorite_id= models.ForeignKey(account_information, on_delete= models.CASCADE)
+    favorite_blog_id = models.ForeignKey(blog_content, on_delete = models.CASCADE)
+ 
     
 
 
