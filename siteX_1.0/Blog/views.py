dda72a5af0227fe7a802a48725ef15c3a672820a
spil3141 <johnj2015.jj22@gmail.com>
2018-10-10 23:18:02 +0900

    Update views.py

--- siteX_1.0/Blog/views.py
+++ siteX_1.0/Blog/views.py
@@ -5,7 +5,7 @@ from .models import account_information, blog_content, favorite_information
 
 def index(request):
     users = account_information.objects.order_by("user_id")
-    output = '<br>'.join([s.user_id for s in users])
+    output = [s.user_id for s in users]
     return render(request,'html/index.html',{"users_all": output})
 
 def About(request):
