bdf92d1ee66511df3ef8c819c7a2babab6930f29
spil3141 cole <johnj2015.jj22@gmail.com>
2018-10-10 23:11:30 +0900

    Bug fix

--- siteX_1.0/Blog/views.py
+++ siteX_1.0/Blog/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
-from Blog.models import account_information, blog_content, favorite_information
+from .models import account_information, blog_content, favorite_information
 
 def index(request):
-    users = account_information.object.all()
+    users = account_information.objects.order_by("user_id")
     return render(request,'html/index.html',{"users_all": users})
 
 def About(request):
